3000a9296569438746848926996390148485691e
CodeConnect4 <168469302+CodeConnect4@users.noreply.github.com>
2024-04-30 01:59:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    mimeTypes: {
-      'application/javascript': ['js', 'jsx'],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: '/path/to/main.js',
     },
 })
